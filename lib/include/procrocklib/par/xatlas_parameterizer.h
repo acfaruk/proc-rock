f917e775e485d949ef6f69b5f97582bf93943776
Faruk Acibal <f_acibal@hotmail.com>
2020-09-09 16:14:12 +0200

    Add some options from xatlas to gui

--- lib/include/procrocklib/par/xatlas_parameterizer.h
+++ lib/include/procrocklib/par/xatlas_parameterizer.h
@@ -4,6 +4,24 @@
 namespace procrock {
 class XAtlasParameterizer : public Parameterizer {
  public:
+  struct ChartOptions {
+    float normalDeviationWeight = 2.0f;
+    float roundnessWeight = 0.01f;
+    float straightnessWeight = 6.0f;
+    float normalSeamWeight = 4.0f;
+    float textureSeamWeight = 0.5f;
+
+    float maxCost = 2.0f;
+    int maxIterations = 1;
+  } chartOptions;
+
+  struct PackOptions {
+    bool bilinear = true;
+    bool blockAlign = true;
+    bool bruteForce = false;
+    int padding = 0;
+  } packOptions;
+
   virtual Configuration getConfiguration() override;
   virtual PipelineStageInfo& getInfo() override;
 
--- lib/src/par/xatlas_parameterizer.cpp
+++ lib/src/par/xatlas_parameterizer.cpp
@@ -33,7 +33,24 @@ std::shared_ptr<Mesh> XAtlasParameterizer::parameterize(Mesh* mesh) {
   meshDecl.indexFormat = xatlas::IndexFormat::UInt32;
 
   xatlas::AddMesh(atlas, meshDecl);
-  xatlas::Generate(atlas);
+
+  xatlas::ChartOptions cOpts;
+  cOpts.normalDeviationWeight = chartOptions.normalDeviationWeight;
+  cOpts.roundnessWeight = chartOptions.roundnessWeight;
+  cOpts.straightnessWeight = chartOptions.straightnessWeight;
+  cOpts.normalSeamWeight = chartOptions.normalSeamWeight;
+  cOpts.textureSeamWeight = chartOptions.textureSeamWeight;
+
+  cOpts.maxCost = chartOptions.maxCost;
+  cOpts.maxIterations = chartOptions.maxIterations;
+
+  xatlas::PackOptions pOpts;
+  pOpts.bilinear = packOptions.bilinear;
+  pOpts.blockAlign = packOptions.blockAlign;
+  pOpts.bruteForce = packOptions.bruteForce;
+  pOpts.padding = packOptions.padding;
+
+  xatlas::Generate(atlas, cOpts, pOpts);
 
   auto& atlasMesh = atlas->meshes[0];
   Eigen::MatrixXd newVertices(atlasMesh.vertexCount, 3);
@@ -67,6 +84,68 @@ std::shared_ptr<Mesh> XAtlasParameterizer::parameterize(Mesh* mesh) {
 
 Configuration XAtlasParameterizer::getConfiguration() {
   Configuration result = getBaseConfiguration();
+
+  Configuration::ConfigurationGroup chartGroup;
+  chartGroup.entry = {"Chart Options",
+                      "Chart Options for XATLAS. The weights define how large the single charts "
+                      "get. If the max cost per chart is reached, then new charts are generated. "
+                      "(Resulting in more charts in the UV mapping.)"};
+
+  chartGroup.floats.emplace_back(Configuration::BoundedEntry<float>{
+      {"Normal Deviation Weight", "Angle between face and avg. chart normal."},
+      &chartOptions.normalDeviationWeight,
+      0.0f,
+      1000.0f});
+  chartGroup.floats.emplace_back(Configuration::BoundedEntry<float>{
+      {"Roundness Weight", "There was no documentation available for this value."},
+      &chartOptions.roundnessWeight,
+      0.0f,
+      1000.1f});
+  chartGroup.floats.emplace_back(Configuration::BoundedEntry<float>{
+      {"Straighness Weight", "There was no documentation available for this value."},
+      &chartOptions.straightnessWeight,
+      0.0f,
+      1000.1f});
+  chartGroup.floats.emplace_back(Configuration::BoundedEntry<float>{
+      {"Normal Seam Weight", "Normals fully respected if > 1000"},
+      &chartOptions.normalSeamWeight,
+      0.0f,
+      1000.1f});
+  chartGroup.floats.emplace_back(Configuration::BoundedEntry<float>{
+      {"Texture Seam Weight", "There was no documentation available for this value."},
+      &chartOptions.textureSeamWeight,
+      0.0f,
+      1000.1f});
+  chartGroup.floats.emplace_back(
+      Configuration::BoundedEntry<float>{{"Max. Cost",
+                                          "If total of all metrics * weights > Max. Cost, then the "
+                                          "chart wont grow bigger. (Creating more charts.)"},
+                                         &chartOptions.maxCost,
+                                         0.0f,
+                                         1000.1f});
+  chartGroup.ints.emplace_back(Configuration::BoundedEntry<int>{
+      {"Max. Iters", "Number of iterations. Higher values result in better charts."},
+      &chartOptions.maxIterations,
+      1,
+      10});
+
+  result.insertToConfigGroups("Chart Options", chartGroup);
+
+  Configuration::ConfigurationGroup packGroup;
+  packGroup.entry = {"Pack Options", "How to pack the charts generated."};
+  packGroup.bools.emplace_back(Configuration::SimpleEntry<bool>{
+      {"Bilinear",
+       "Leave space around the charts for texels that would be sampled by bilinear filtering."},
+      &packOptions.bilinear});
+  packGroup.bools.emplace_back(Configuration::SimpleEntry<bool>{
+      {"Block Align", "Align charts to 4x4 blocks. Also improves packing speed."},
+      &packOptions.blockAlign});
+  packGroup.bools.emplace_back(Configuration::SimpleEntry<bool>{
+      {"Brute Force", "Gives best result, but is slower."}, &packOptions.bruteForce});
+  packGroup.ints.emplace_back(Configuration::BoundedEntry<int>{
+      {"Padding", "Pixels to pad charts with."}, &packOptions.padding, 0, 10});
+
+  result.insertToConfigGroups("Pack Options", packGroup);
   return result;
 }
 
