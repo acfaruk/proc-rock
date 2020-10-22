e56c0990d4014214d72fe75684aeada27b9134cf
Faruk Acibal <f_acibal@hotmail.com>
2020-10-22 10:33:38 +0200

    Add gts library
    - commit to check CI builds

--- setup.bat
+++ setup.bat
@@ -1,9 +1,10 @@
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-git checkout tags/2020.07
+git checkout -b procrock tags/2020.07
+git apply ../vcpkg.patch
 call bootstrap-vcpkg.bat
 
 SET VCPKG_DEFAULT_TRIPLET=x64-windows
 
-vcpkg install glfw3 tinyfiledialogs imgui[opengl3-glew-binding,glfw-binding] glew glm libigl libnoise cimg nlohmann-json
+vcpkg install glfw3 tinyfiledialogs imgui[opengl3-glew-binding,glfw-binding] glew glm libigl libnoise cimg nlohmann-json gts
 pause
--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-git checkout tags/2020.07
+git checkout -b procrock tags/2020.07
+git apply ../vcpkg.patch
 
 ./bootstrap-vcpkg.sh
 ./vcpkg update
-./vcpkg install glfw3 tinyfiledialogs imgui[opengl3-glew-binding,glfw-binding] glew glm libigl libnoise cimg nlohmann-json
+./vcpkg install glfw3 tinyfiledialogs imgui[opengl3-glew-binding,glfw-binding] glew glm libigl libnoise cimg nlohmann-json gts
