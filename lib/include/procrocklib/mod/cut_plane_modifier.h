c44d43d5cd391129e3d133de1bb517b2f9be2441
Faruk Acibal <f_acibal@hotmail.com>
2020-11-18 14:28:25 +0100

    Add noise to cut plane modifier
    allows the user to apply a noise to the plane before cutting the rock -> approximates weak planes breaking on the cut

--- lib/include/procrocklib/mod/cut_plane_modifier.h
+++ lib/include/procrocklib/mod/cut_plane_modifier.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <procrocklib/configurables/noise_graph.h>
 #include <procrocklib/modifier.h>
 
 namespace procrock {
@@ -10,10 +11,17 @@ class CutPlaneModifier : public Modifier {
   Eigen::Vector3f translation{0, 0, 0};
   Eigen::Vector3f rotation{0.3, 0, 1.7};
 
+  bool useNoise = false;
+  NoiseGraph noiseGraph;
+  int resolution = 7;
+  float scale = 0.15f;
+
   virtual std::shared_ptr<Mesh> modify(Mesh& mesh) override;
   virtual PipelineStageInfo& getInfo() override;
 
  private:
+  void noisifyCube(Mesh& mesh);
+
   PipelineStageInfo info{PipelineStageNames_Mod[PipelineStage_Mod_CutPlane],
                          "Cut the mesh along a Plane", PipelineStageType::Modifier,
                          PipelineStage_Mod_CutPlane};
--- lib/src/mod/cut_plane_modifier.cpp
+++ lib/src/mod/cut_plane_modifier.cpp
@@ -2,6 +2,7 @@
 
 #include <CGAL/Polygon_mesh_processing/corefinement.h>
 #include <igl/per_vertex_normals.h>
+#include <igl/upsample.h>
 #include <math.h>
 
 #include <Eigen/Geometry>
@@ -12,7 +13,7 @@ namespace procrock {
 CutPlaneModifier::CutPlaneModifier() {
   Configuration::ConfigurationGroup group;
 
-  group.entry = {"General Settings", "Set how the plane should be positioned."};
+  group.entry = {"General Settings", "Set general properties"};
   group.float3s.emplace_back(Configuration::BoundedEntry<Eigen::Vector3f>{
       {"Translation", "Translate the plane in space."}, &translation, {-2, -2, -2}, {2, 2, 2}});
   group.float3s.emplace_back(
@@ -21,7 +22,23 @@ CutPlaneModifier::CutPlaneModifier() {
                                                    {0, 0, 0},
                                                    {2 * M_PI, 2 * M_PI, 2 * M_PI}});
 
+  auto showNoiseFunc = [&] { return useNoise; };
+
+  group.bools.emplace_back(Configuration::SimpleEntry<bool>{
+      {"Noisy Surface", "Use a noise graph for the surface of the plane."}, &useNoise});
+
+  Configuration::ConfigurationGroup noiseGroup;
+  noiseGroup.entry = {"Noise Settings", "Settings for the applied noise", showNoiseFunc};
+
+  noiseGroup.noiseGraphs.emplace_back(Configuration::SimpleEntry<NoiseGraph>{
+      {"Noise Graph", "The graph to define the applied noise."}, &noiseGraph});
+  noiseGroup.ints.emplace_back(Configuration::BoundedEntry<int>{
+      {"Resolution", "Resolution of mesh to have before applying noise."}, &resolution, 5, 9});
+  noiseGroup.floats.emplace_back(Configuration::BoundedEntry<float>{
+      {"Scale", "Scaling of noise depth in plane."}, &scale, 0.001f, 0.2f});
+
   config.insertToConfigGroups("General", group);
+  config.insertToConfigGroups("Noise", noiseGroup);
 }
 
 std::shared_ptr<Mesh> CutPlaneModifier::modify(Mesh& mesh) {
@@ -30,9 +47,9 @@ std::shared_ptr<Mesh> CutPlaneModifier::modify(Mesh& mesh) {
 
   Eigen::Matrix<double, 8, 3> vertices;
 
-  double half_length = 100;
-  double half_height = 100;
-  double half_depth = 100;
+  double half_length = 10;
+  double half_height = 10;
+  double half_depth = 10;
 
   vertices.row(0) << half_length, -half_height, -half_depth;
   vertices.row(1) << half_length, half_height, -half_depth;
@@ -68,6 +85,10 @@ std::shared_ptr<Mesh> CutPlaneModifier::modify(Mesh& mesh) {
   cubeMesh->vertices = vertices;
   cubeMesh->faces = faces;
 
+  if (useNoise) {
+    noisifyCube(*cubeMesh);
+  }
+
   utils::transform(*cubeMesh, translation, {1, 1, 1}, rotation);
 
   utils::CGAL_Mesh secondBoolMesh;
@@ -83,5 +104,30 @@ std::shared_ptr<Mesh> CutPlaneModifier::modify(Mesh& mesh) {
   igl::per_vertex_normals(result->vertices, result->faces, result->normals);
   return result;
 }
+
+void CutPlaneModifier::noisifyCube(Mesh& mesh) {
+  // Subdivide cube first
+  igl::upsample(mesh.vertices, mesh.faces, resolution);
+
+  // Recalculate normals
+  igl::per_vertex_normals(mesh.vertices, mesh.faces, mesh.normals);
+
+  // Apply noise
+  auto module = evaluateGraph(noiseGraph);
+
+  for (int i = 0; i < mesh.vertices.rows(); i++) {
+    auto pos = mesh.vertices.row(i);
+    float amount = (module->GetValue(pos(0), pos(1), pos(2)) + 1) / 2.0;
+    amount = std::min(1.0f, std::max(0.0f, amount));
+    auto translation =
+        Eigen::Translation<double, 3>((scale * amount * mesh.normals.row(i).normalized()));
+    auto transformedPos = translation * Eigen::Vector3d(mesh.vertices.row(i));
+    mesh.vertices.row(i) = transformedPos;
+  }
+
+  // Recalculate normals
+  igl::per_vertex_normals(mesh.vertices, mesh.faces, mesh.normals);
+}
+
 PipelineStageInfo& CutPlaneModifier::getInfo() { return info; }
 }  // namespace procrock
